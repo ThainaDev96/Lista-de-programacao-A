2212d03d125713060900e9ff5195ad28170180c5
Thaina Ribeiro <rthaina776@gmail.com>
2026-02-09 23:06:28 -0300

    Rename Questao4_Thaina_Ribeiro_Bairros.java to Questao4_Thaina_Ribeiro.java

--- Prova/Questao4_Thaina_Ribeiro.java
+++ Prova/Questao4_Thaina_Ribeiro.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package provathainaribeirobairros;
+package provathainaribeiro;
 
 import javax.swing.JOptionPane;
 
@@ -53,3 +53,4 @@ JOptionPane.showMessageDialog(null, "O valor da area do trapezio eh: " + areaTra
 }
     
    
+
