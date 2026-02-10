337c43f891cdfae6c4cb60eb76155f8102d7425c
Thaina Ribeiro <rthaina776@gmail.com>
2026-02-10 17:09:10 -0300

    Add files via upload

---
+++ protrabalho2/ProTrabalho18.java
@@ -0,0 +1,54 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @authorThainaRibeiro
+ */
+public class ProTrabalho18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*18. Escreva um main Java que solicita 8 inteiros ao usuário e guarda esses valores
+        em um array. Depois o programa deve descobrir e exibir qual a posiçao do elemento de maior valor.
+        */
+        
+        int[] valores = new int[8]; 
+        int maiorValor = valores[0];
+        int posicaoMaior = 0;
+        
+        for (int i = 0; i < valores.length; i++) {
+            valores[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o valor inteiro " + (i + 1)));
+        }
+
+        for (int i = 1; i < valores.length; i++) {
+            if (valores[i] > maiorValor) {
+                maiorValor = valores[i];
+                posicaoMaior = i;
+            }
+        }
+        System.out.print("Valores digitados: ");
+        for (int i = 0; i < valores.length; i++) {
+            System.out.print(valores[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("O maior valor eh " + maiorValor + " e está na posição " + posicaoMaior);
+    }
+}
+
+
+
+        
+   
+
---
+++ protrabalho2/Protrabalho6.java
@@ -0,0 +1,47 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ThainaRibeiro
+ */
+public class Protrabalho6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*6.Escreva que leia um array de inteiros e retorna a quantidade de elementos do array
+        que são números negativos*/
+        
+        String[] nInteiros = new String[5];
+        int nNegativos=0;
+        
+        for (int i = 0; i < nInteiros.length; i++) {
+            nInteiros[i] = JOptionPane.showInputDialog(null, "Informe numeros inteiros)");
+        
+            
+            int num = Integer.parseInt(nInteiros[i]);
+            if (num < 0) {
+                nNegativos++;
+                
+            }
+        }
+        // Saída de dados
+         System.out.print("Números inteiros: ");
+        for (int i = 0; i < nInteiros.length; i++) {
+            System.out.print(nInteiros[i] + " ");
+        }
+        
+        System.out.println("Quantidade de numeros negativos: " + nNegativos);
+         
+    }
+    
+}
