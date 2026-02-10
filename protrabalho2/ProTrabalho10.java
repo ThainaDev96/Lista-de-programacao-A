a9d2d26b1e57109ab3be413e6e1fe484836fc0e6
Thaina Ribeiro <rthaina776@gmail.com>
2026-02-10 16:37:01 -0300

    Update ProTrabalho10.java

--- protrabalho2/ProTrabalho10.java
+++ protrabalho2/ProTrabalho10.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @authorThainaRibeiroBairros
+ * @authorThainaRibeiro
  */
 public class ProTrabalho10 {
 
@@ -45,5 +45,6 @@ public class ProTrabalho10 {
     }
 }
     
+
 
        
